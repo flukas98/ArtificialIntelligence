7c4e883ea353f14dbf20595f655a1ef827811835
Filip Lukas <52679825+flukas98@users.noreply.github.com>
2022-02-24 14:08:44 +0000

    Add files via upload

---
+++ Task 1/Lab1/Lab1/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab1
+{
+    public class Node
+    {
+        public string State { get; private set; }
+
+        public float TotalCost { get; private set; }
+
+        public Node ParentNode { get; private set; }
+
+        public static Node Create(string state, float totalCost, Node parentNode)
+        {
+            var node = new Node()
+            {
+                State = state,
+                TotalCost = totalCost,
+                ParentNode = parentNode
+            };
+
+            return node;
+        }
+    }
+}
